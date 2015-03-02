07ba3ebc3a77c79bddf1a09d1d98275b75a5222f
steve bi <sbi@yesvideo.com>
2015-03-02 08:13:24 -0800

    test

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'haml-rails'
 gem 'pry-rails'
+gem 'simple_form'
 
 gem 'bootstrap-sass'
 gem 'jquery-rails'
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,5 +4,6 @@ class VisitorsController < ApplicationController
     @owner.first_name = "first"
     @owner.last_name = "last"
     @owner.birthdate = Date.new(1968, 3, 18)
+
   end
 end
